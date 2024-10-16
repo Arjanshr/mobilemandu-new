cc1128b4f91e11c816dbdce0498cb47c89816432
Arjanshr <sirarjan@gmail.com>
2024-10-16 15:57:39 +0545

    Bug fix

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,10 +13,18 @@ class CorsMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
+
     public function handle($request, Closure $next)
     {
+        if ($request->isMethod('options')) {
+            return response()->json([], 200)
+                ->header('Access-Control-Allow-Origin', 'https://front-test.mobilemandu.com')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
+
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://front-test.mobilemandu.com') // Your frontend URL
+            ->header('Access-Control-Allow-Origin', 'https://front-test.mobilemandu.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
