25ae74ec172c4aaa956facb25720f5f2f9f65152
Arjanshr <sirarjan@gmail.com>
2024-11-26 13:40:24 +0545

    profile update bug fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -24,8 +24,8 @@ class UserController extends BaseController
 
     public function editProfile(UserRequest $request)
     {
-        return $request;
         $user = auth()->user();
+        return $user;
         $user->name = $request->name;
         $user->email = $request->email;
         $user->phone = $request->phone;
