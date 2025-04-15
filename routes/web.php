526ce7d37345d707bbf67fc7298906fd83803ba8
Arjanshr <sirarjan@gmail.com>
2025-04-15 12:50:46 +0545

    Config cleared

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,12 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontController::class, 'home'])->name('front.home');
+Route::get('/clear-config', function () {
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    return 'Config cache cleared!';
+});
+
 Route::get('/storage-link', function () {
     Artisan::call('storage:link');
     return 'storage:linked';
