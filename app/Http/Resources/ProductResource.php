f3b19cc9103c17bae7ad182822199e01a0dee6f6
Arjanshr <sirarjan@gmail.com>
2025-04-16 11:27:40 +0545

    test reverted

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -17,8 +17,6 @@ class ProductResource extends JsonResource
         return [
             "id" => $this->id,
             "name" => $this->name,
-            "link" => 'https://www.mobilemandu.com/products/'.$this->slug,
-            "description" => $this->description,
             "slug" => $this->slug,
             "rating" => $this->getAverageRating(),
             "discounted_amount" => $this->discounted_price,
