0dd2cf5f727f231637adb324ee47858e2561482a
Arjanshr <sirarjan@gmail.com>
2025-05-13 12:32:41 +0545

    Handler file added

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    protected $levels = [];
+
+    protected $dontReport = [];
+
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof HttpException) {
+            return response()->json([
+                'success' => false,
+                'message' => $exception->getMessage(),
+            ], $exception->getStatusCode() ?: 400);
+        }
+
+        if ($request->expectsJson()) {
+            return response()->json([
+                'success' => false,
+                'message' => $exception->getMessage(),
+            ], method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500);
+        }
+
+        return parent::render($request, $exception);
+    }
+}
